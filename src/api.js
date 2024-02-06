fc02b2229d8586f7856003096c63e92204cd02f9
Squard974 <werr.sebastien@gmail.com>
2024-02-06 04:06:34 +0100

    étape 3 terminée (fonctions codées + images)

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-
+import {createHash} from 'node:crypto'
 
 /**
  * Récupère les données de l'endpoint en utilisant les identifiants
@@ -7,9 +7,42 @@
  * @return {Promise<json>}
  */
 export const getData = async (url) => {
-    let baseUrl = "/https://gateway.marvel.com:443/v1/public/characters?"
-    ts
-    ts={{maintenant}}&apikey={{apikey}}&hash={{hachage}}"
+    let ts = new Date().getTime()
+    let publicKey = "361124b7737967445b1ee399a9fd3846"
+    let privateKey = "875674f8520063d42315708994ac56cfe874eda7"
+    let hash = await getHash(ts, privateKey, publicKey)
+
+    let completeUrl = url + "?ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash
+    console.log(completeUrl)
+    //https://gateway.marvel.com/v1/public/characters?ts=1707185049623&apikey=361124b7737967445b1ee399a9fd3846&hash=7d31418eb53dbd0016760110f2eb03a1
+    return await fetch(completeUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP! Statut : ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Filtrer les personnages avec une thumbnail valide
+            const filteredCharacters = data.data.results.filter(character =>
+                character.thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+            );
+
+            console.log(filteredCharacters);
+
+            // Tableau des personnages avec portraix_xlarge
+            const charactersWithImageUrl = filteredCharacters.map(character => ({
+                id: character.id,
+                name: character.name,
+                imageUrl: `${character.thumbnail.path}/portrait_xlarge.${character.thumbnail.extension}`
+            }));
+
+            console.log(charactersWithImageUrl);
+
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données:', error);
+        })
 }
 
 /**
@@ -20,6 +53,7 @@ export const getData = async (url) => {
  * @param timestamp
  * @return {Promise<ArrayBuffer>} en hexadecimal
  */
-export const getHash = async (publicKey, privateKey, timestamp) => {
-    // A compléter
+export const getHash = async (timestamp, privateKey, publicKey) => {
+    return createHash('md5').update(timestamp + privateKey + publicKey).digest('hex');
+
 }
